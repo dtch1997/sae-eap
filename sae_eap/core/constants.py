ee6d7cbee50835a80f848f69c62e67d6edc3577a
Daniel Tan <dtch1997@users.noreply.github.com>
2024-06-14 15:06:28 +0100

    EAP working

--- sae_eap/core/constants.py
+++ sae_eap/core/constants.py
@@ -1,6 +1,6 @@
 import pathlib
 
-GPT_2_SMALL: str = "gpt2"
-ALL_GPT_2_SMALL_LAYERS: list[int] = list(range(12))
+GPT_2_SMALL_MODEL_NAME: str = "gpt2"
+GPT_2_SMALL_ALL_LAYERS: list[int] = list(range(12))
 
 ProjectDir = pathlib.Path(__file__).parent.parent
--- tests/pretrained/test_load_attn_saes.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from circuit_finder.pretrained import load_attn_saes
-
-
-# TODO: test for all layers
-@pytest.mark.parametrize("layer", [11])
-def test_load_attn_saes(layer: int):
-    sae = load_attn_saes(layers=[layer])
-    assert sae[layer] is not None
