f7600feab43eab91db5fe8b644c3e9c88a0b766f
Daniel Tan <dtch1997@users.noreply.github.com>
2024-06-18 15:46:41 +0100

    working tests for indexer

--- sae_eap/graph/index.py
+++ sae_eap/graph/index.py
@@ -1,29 +1,54 @@
 from sae_eap.graph import Graph, Node
 
+Index = tuple[int, ...]
+
+
+def get_indices(start, end, step: int = 1) -> Index:
+    return tuple(range(start, end, step))
+
 
 class GraphIndexer:
     """A class to index nodes in a graph."""
 
+    node_to_act_index: dict[Node, Index]
+    node_to_grad_index: dict[Node, Index]
+
     def __init__(self, graph: Graph):
+        self.reset_index(graph)
+
+    def reset_index(self, graph: Graph):
         node_to_act_index = {}
+        curr_act_index = 0
+        for node in graph.nodes:
+            n_node_outputs = len(node.get_out_hooks())
+            node_to_act_index[node] = get_indices(
+                curr_act_index, curr_act_index + n_node_outputs
+            )
+            curr_act_index += n_node_outputs
+        self._n_act_index = curr_act_index
+        self.node_to_act_index = node_to_act_index
+
         node_to_grad_index = {}
-        for i, node in enumerate(graph.nodes):
-            node_to_act_index[node] = i
-            for node_input in enumerate(node.inputs):
-                node_to_grad_index[node_input] = len(node_to_grad_index)
+        curr_n_grad_index = 0
+        for node in graph.nodes:
+            n_node_inputs = len(node.get_in_hooks())
+            node_to_grad_index[node] = get_indices(
+                curr_n_grad_index, curr_n_grad_index + n_node_inputs
+            )
+            curr_n_grad_index += n_node_inputs
+        self._n_grad_index = curr_n_grad_index
+        self.node_to_grad_index = node_to_grad_index
 
     @property
     def n_act_index(self):
-        raise NotImplementedError
+        return self._n_act_index
 
     @property
     def n_grad_index(self):
-        raise NotImplementedError
+        return self._n_grad_index
 
-    def get_act_index(self, src_node: Node):
-        """Get the forward index of a node in the graph."""
-        raise NotImplementedError
+    def get_act_index(self, node: Node) -> Index:
+        return self.node_to_act_index[node]
 
-    def get_grad_index(graph: Graph, dest_node: Node):
-        """Get the backward index of a node in the graph."""
-        raise NotImplementedError
+    def get_grad_index(self, node: Node) -> Index:
+        return self.node_to_grad_index[node]
--- tests/unit/graph/test_index.py
+++ tests/unit/graph/test_index.py
@@ -1,11 +1,59 @@
-import pytest
 from sae_eap.graph.build import build_graph
+from sae_eap.graph.index import GraphIndexer
 
 
-@pytest.mark.xfail
-def test_graph_indexer(ts_model):
+def get_n_act_index_of_model_only_graph(cfg):
+    """Get the number of activation indices in a graph without any SAEs.
+
+    Formula:
+    - InputNode: 1
+    - Attention Node: n_head x n_layers
+    - MLP Node: n_layers
+    - Logit Node: 0
+    """
+    return 1 + cfg.n_layers * (cfg.n_heads + 1)
+
+
+def get_n_grad_index_of_model_only_graph(cfg):
+    """Get the number of gradient indices in a graph without any SAEs.
+
+    Formula:
+    - InputNode: 0
+    - Attention Node: 3 x n_head x n_layers
+    - MLP Node: n_layers
+    - Logit Node: 1
+    """
+    return cfg.n_layers * (3 * cfg.n_heads + 1) + 1
+
+
+def test_graph_indexer_basic(ts_model):
     graph = build_graph(ts_model)
-    assert graph.n_forward == 1 + graph.cfg["n_layers"] * (graph.cfg["n_heads"] + 1)
-    assert (
-        graph.n_backward == graph.cfg["n_layers"] * (3 * graph.cfg["n_heads"] + 1) + 1
-    )
+    graph_indexer = GraphIndexer(graph)
+    assert graph_indexer.n_act_index == get_n_act_index_of_model_only_graph(graph.cfg)
+    assert graph_indexer.n_grad_index == get_n_grad_index_of_model_only_graph(graph.cfg)
+
+    for node in graph.nodes:
+        act_index = graph_indexer.get_act_index(node)
+        grad_index = graph_indexer.get_grad_index(node)
+        assert len(act_index) == len(node.get_out_hooks())
+        assert len(grad_index) == len(node.get_in_hooks())
+
+
+def test_graph_index_does_not_repeat(ts_model):
+    graph = build_graph(ts_model)
+    graph_indexer = GraphIndexer(graph)
+
+    act_indices_seen = set()
+    grad_indices_seen = set()
+
+    for node in graph.nodes:
+        act_index = graph_indexer.get_act_index(node)
+        grad_index = graph_indexer.get_grad_index(node)
+
+        for a in act_index:
+            assert a not in act_indices_seen
+            act_indices_seen.add(act_index)
+
+        for g in grad_index:
+            assert g not in grad_indices_seen
+            grad_indices_seen.add(grad_index)
