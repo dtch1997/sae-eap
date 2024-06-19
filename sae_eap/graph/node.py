64c2fef90302ee09e7e363e961da6139b555e1f1
Daniel Tan <dtch1997@users.noreply.github.com>
2024-06-19 14:45:52 +0100

    minor

--- sae_eap/graph/node.py
+++ sae_eap/graph/node.py
@@ -23,7 +23,7 @@ class TensorNode(Node):
     """A node corresponding to a tensor in the model's computational graph."""
 
     hook: HookName
-    head_index: int = 0
+    head_index: int | None = None
 
     @property
     def is_src(self) -> bool:
@@ -58,87 +58,3 @@ class DestNode(TensorNode):
     @property
     def is_dest(self) -> bool:
         return True
-
-
-# class LogitNode(Node):
-#     """A node corresponding to the output logits of the model."""
-
-#     def __init__(self, n_layers: int):
-#         name = "logits"
-#         super().__init__(
-#             name=name,
-#             layer=n_layers - 1,
-#             in_hook=f"blocks.{n_layers - 1}.hook_resid_post",
-#             out_hook=(),
-#         )
-
-
-# class MLPNode(Node):
-#     """A node corresponding to an MLP block in the model."""
-
-#     def __init__(self, layer: int):
-#         name = f"m{layer}"
-#         super().__init__(
-#             name=name,
-#             layer=layer,
-#             in_hook=f"blocks.{layer}.hook_mlp_in",
-#             out_hook=f"blocks.{layer}.hook_mlp_out",
-#         )
-
-
-# class AttentionNode(Node):
-#     """A node corresponding to an attention head in the model.
-
-#     NOTE: We have one node per head.
-#     """
-
-#     head: int
-
-#     def __init__(self, layer: int, head: int):
-#         name = f"a{layer}.h{head}"
-#         self.head = head
-#         super().__init__(
-#             name=name,
-#             layer=layer,
-#             in_hook=tuple([f"blocks.{layer}.hook_{letter}_input" for letter in "qkv"]),
-#             out_hook=f"blocks.{layer}.attn.hook_result",
-#         )
-
-
-# class InputNode(Node):
-#     """A node corresponding to the input of the model."""
-
-#     def __init__(self):
-#         name = "input"
-#         super().__init__(
-#             name=name,
-#             layer=0,
-#             in_hook=(),
-#             out_hook="hook_embed",
-#         )  # "blocks.0.hook_resid_pre", index)
-
-
-# # SAE nodes.
-# # NOTE: Currently hardcoded to resid_pre
-
-
-# class SAEReconstructionNode(Node):
-#     def __init__(self, layer: int):
-#         name = f"sae_recons{layer}"
-#         super().__init__(
-#             name=name,
-#             layer=layer,
-#             in_hook=f"blocks.{layer}.hook_resid_pre.hook_sae_input",
-#             out_hook=f"blocks.{layer}.hook_resid_pre.hook_sae_recons",
-#         )
-
-
-# class SAEErrorNode(Node):
-#     def __init__(self, layer: int):
-#         name = f"sae_error{layer}"
-#         super().__init__(
-#             name=name,
-#             layer=layer,
-#             in_hook=f"blocks.{layer}.hook_resid_pre.hook_sae_input",
-#             out_hook=f"blocks.{layer}.hook_resid_pre.hook_sae_error",
-#         )
